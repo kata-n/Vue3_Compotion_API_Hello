bbca56b828a94ae078e8896e4f4805564cf331b7
kata-n <kata.kawa813@gmail.com>
2020-08-21 08:08:33 +0900

    first message

---
+++ src/components/sample.vue
@@ -0,0 +1,39 @@
+<template>
+  <h3>{{ state.messageOne }}</h3>
+  <h3>{{ messageTwo }}</h3>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  setup() {
+    const state = reactive<{ messageOne: string }>({
+      messageOne: 'Hello'
+    });
+    const messageTwo = ref<string>("こんにちは");
+
+    return {
+      state,
+      messageTwo
+    };
+  }
+});
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue';
+import VueCompositionApi from "@vue/composition-api";
+Vue.use(VueCompositionApi);
 import App from './App.vue'
 
 createApp(App).mount('#app')
