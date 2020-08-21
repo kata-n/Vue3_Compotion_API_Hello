dfa10c56062f0ce58892246e5a9cfe76b9b842a7
kata-n <kata.kawa813@gmail.com>
2020-08-21 20:32:11 +0900

    composition APiについて

--- README.md
+++ README.md
@@ -1,5 +1,25 @@
 # vue3_composition_api
 
+### Composition APIとメリット
+
+コンポーネントのロジックを再利用しやすくするための仕組み。
+
+自分で作ったロジックをコンポーネント間で再利用できる様にする
+
+Vue.jsでのプロジェクトが大規模になると、コンポーネントが肥大化してしまい読み解くことが困難になる。
+
+### Composition APIのデメリット
+
+ロジックをある程度自由に書ける分、自由すぎて共通化されない。
+
+チームで開発する際には設計などのルール決めなどが大切になる。
+
+### Composition API参考サイト
+
+なぜ、Vue Composition APIを使うのか、理解する【メリット/デメリットまとめ】
+
+https://qiita.com/karamage/items/7721c8cac149d60d4c4a
+
 ## Project setup
 ```
 yarn install
---
+++ src/components/incremental.vue
@@ -0,0 +1,40 @@
+<template>
+  <button @click="increment">
+    現在のカウント数 {{ state.count }}、2倍したカウント数 {{ state.double }}
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  setup() {
+    const state = reactive<{ messageOne: string }>({
+      messageOne: 'Hello'
+    });
+    const messageTwo = ref<string>("こんにちは");
+
+    return {
+      state,
+      messageTwo
+    };
+  }
+});
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
